5eb8c5417c8d7d6b5367b985155ff7936d85c8a6
Zanco1993 <zanconatofabio@gmail.com>
2022-03-08 14:09:34 +0100

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.layout')
+
+@section('header')
+<header class="text-center py-5">
+    <h1>LISTA FUMETTI</h1>
+</header>
+@endsection
+
+@section('main')
+<div class="container">
+    <ul>
+        <li>prova</li>
+        <li>prova</li>
+        <li>prova</li>
+        <li>prova</li>
+        <li>prova</li>
+    </ul>
+</div>
+
+@endsection
